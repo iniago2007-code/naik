8e4b9ea33d27b28ec2c6418758d78ff92b6b7f67
iniago2007-code <iniago2007@gmail.com>
2026-03-12 00:17:04 +0100

    Add src/App.js for the React project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div>
+      <h1>Africa Production Recommender</h1>
+      <Search />
+    </div>
+  );
+}
+export default App;
